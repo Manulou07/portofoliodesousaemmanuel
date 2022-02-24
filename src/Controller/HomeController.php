9ec0d3a307f01deabd4281ecf5762e92cd09a0e6
emmanuel de sousa <desousa.emmanuel@hotmail.fr>
2022-02-24 10:48:43 +0100

    formulaire de contact

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
+use Symfony\Component\Mime\Address;
 use App\Repository\AproposRepository;
-use App\Repository\CompetenceRepository;
 use App\Repository\ProjetsRepository;
+use App\Repository\CompetenceRepository;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,17 +17,47 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(CompetenceRepository $competenceRepository, AproposRepository $aproposRepository, ProjetsRepository $projetsRepository): Response
+    public function index(Request $request, MailerInterface $mailer, CompetenceRepository $competenceRepository, AproposRepository $aproposRepository, ProjetsRepository $projetsRepository): Response
     {
 
-        $technologies = $competenceRepository->findAll();
+      
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData(); 
+            $email = (new TemplatedEmail())
+                ->from(new Address($contact['email'], $contact['telephone'], $contact['prenom'] . ' ' . $contact['nom']))
+                ->to(new Address('desousa.emmanuel@gmail.com'))
+                ->subject('PORTOFOLIO - demande de contact - ' . $contact['objet'])
+                ->htmlTemplate('contact/index.html.twig') 
+                ->context([ 
+                    'prenom' => $contact['prenom'],
+                    'nom' => $contact['nom'],
+                    'adresseEmail' => $contact['email'],
+                    'telephone' => $contact['telephone'],
+                    'objet' => $contact['objet'],
+                    'message' => $contact['message'],
+                ]);
+           
+            $mailer->send($email);
+            $this->addFlash('success', 'Votre message a bien été envoyé');
+            return $this->redirectToRoute('home');
+        }
+        
+       $technologies = $competenceRepository->findAll();
         $apropos = $aproposRepository->findAll();
         $projets = $projetsRepository->findAll();
+      
 
+        
         return $this->render('home/index.html.twig',[
+            'projets' => $projets,
             'competences' => $technologies, 
             'apropos' => $apropos,
-            'projets' => $projets
+            'contactForm' => $form->createView(),
         ]);
     }
+
+    
 }
